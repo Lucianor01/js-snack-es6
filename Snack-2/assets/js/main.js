101fb9cfba9ef29ac0da7d1b9a3dd63c3e1b638c
Lucianor01 <lucianorosario02@gmail.com>
2023-04-14 16:56:48 +0200

    Creazione-array-object e funzione numero random

--- Snack-2/assets/js/main.js
+++ Snack-2/assets/js/main.js
@@ -6,3 +6,35 @@
 * Punti fatti e falli subiti.
 * Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. 
 */
+
+const squadraCalcio = [
+    {
+        nome: 'Juventus',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Inter',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Milan',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Napoli',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+]
+
+function numeroRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
