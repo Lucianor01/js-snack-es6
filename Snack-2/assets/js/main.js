2ca63aa96731cbeda9c307d5012cca56b7a364f9
Lucianor01 <lucianorosario02@gmail.com>
2023-04-14 17:01:53 +0200

    forEach- arrowFunction-squadra

--- Snack-2/assets/js/main.js
+++ Snack-2/assets/js/main.js
@@ -38,3 +38,10 @@ const squadraCalcio = [
 function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+squadraCalcio.forEach( squadra => {
+    squadra.puntiFatti = numeroRandom(0, 100)
+    squadra.falliSubiti = numeroRandom(0, 50)
+    console.log(squadra.puntiFatti);
+    console.log(squadra.falliSubiti);
+})
