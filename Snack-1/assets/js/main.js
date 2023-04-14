31fd6b4d2f450d210289a0cf23b22607564007ce
Lucianor01 <lucianorosario02@gmail.com>
2023-04-14 15:43:11 +0200

    Creazione-array-oggetti

--- Snack-1/assets/js/main.js
+++ Snack-1/assets/js/main.js
@@ -4,3 +4,26 @@
 * Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 * Stampare a schermo la bici con peso minore utilizzando destructuring e template literal 
 */
+
+const bici = [
+    {
+        nome: 'Luca',
+        peso: '30',
+    },
+    {
+        nome: 'Marco',
+        peso: '20',
+    },
+    {
+        nome: 'Maria',
+        peso: '15',
+    },
+    {
+        nome: 'Sara',
+        peso: '10',
+    },
+    {
+        nome: 'Mattia',
+        peso: '23',
+    }
+]
